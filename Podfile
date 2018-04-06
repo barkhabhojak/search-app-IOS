19933a71df1101dfcf491b5cffb4cde58f5e5ba7
Barkha Bhojak <barkha@Barkhas-Macbook.local>
2018-04-06 09:49:26 -0700

    base form with category picker

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SearchIOS' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SearchIOS
+
+pod 'Alamofire'
+pod 'AlamofireSwiftyJSON'
+pod ’McPicker'
+pod 'SwiftSpinner'
+pod 'EasyToast'
+pod 'Cosmos'
+pod 'GooglePlaces'
+
+end
--- SearchIOS/ViewController.swift
+++ SearchIOS/ViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import McPicker
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mcTextField: McTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let data: [[String]] = [["Default", "Airport","Amusement Park", "Aquarium", "Art Gallery", "Bakery", "Bar", "Beauty Salon", "Bowling Alley", "Bus Station", "Cafe", "Campground", "Car Rental", "Casino", "Lodging", "Movie Theater", "Museum", "Night Club", "Park", "Parking", "Restaurant", "Shopping Mall", "Stadium", "Subway Station", "Taxi Stand", "Train Station", "Transit Agency", "Zoo"]]
+        let mcInputView = McPicker(data: data)
+        mcTextField.inputViewMcPicker = mcInputView
+        mcTextField.doneHandler = { [weak mcTextField] (selections) in
+            mcTextField?.text = selections[0]!
+            //do something if user selects an option and taps done
+        }
+            mcTextField.cancelHandler = { [weak mcTextField] in
+                //do something if user cancels
+            }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
