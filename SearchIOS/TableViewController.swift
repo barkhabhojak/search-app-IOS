234cd515d9292dce9cb2213f6b26273fbd0bf881
Barkha Bhojak <barkha@Barkhas-Macbook.local>
2018-04-07 15:21:24 -0700

    enable/disable btn

--- SearchIOS/TableViewController.swift
+++ SearchIOS/TableViewController.swift
@@ -11,14 +11,17 @@ import SwiftSpinner
 import Alamofire
 import AlamofireSwiftyJSON
 
-class TableViewController: UIViewController {
+class TableViewController: UIViewController,UIGestureRecognizerDelegate {
     
     var url = ""
     @IBOutlet weak var tblJSON: UITableView!
     var arrRes = [[String:AnyObject]]()
-
+    @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var prevBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         self.tblJSON.tableFooterView = UIView(frame: CGRect.zero)
         tblJSON.rowHeight = 70
 //        tblJSON.separatorStyle = UITableViewCellSeparatorStyle.singleLine
@@ -36,13 +39,18 @@ class TableViewController: UIViewController {
                 if let resData = places!["results"].arrayObject {
                     self.arrRes = resData as! [[String:AnyObject]]
                 }
+                self.prevBtn.isEnabled = false;
                 self.tblJSON.reloadData()
             }
             else if places!["status"] == "ZERO_RESULTS" {
                 self.view.showToast("No results", position: .bottom, popTime: 3, dismissOnTap: false, bgColor: UIColor.black, textColor: UIColor.white, font: UIFont.boldSystemFont(ofSize: 19))
+                self.prevBtn.isEnabled = false
+                self.nextBtn.isEnabled = false
             }
             else {
                 self.view.showToast("Error in retrieving details", position: .bottom, popTime: 3, dismissOnTap: false, bgColor: UIColor.black, textColor: UIColor.white, font: UIFont.boldSystemFont(ofSize: 19))
+                self.prevBtn.isEnabled = false
+                self.nextBtn.isEnabled = false
             }
 
             SwiftSpinner.hide()
@@ -83,7 +91,7 @@ extension TableViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView,didSelectRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at: indexPath) as! CustomTableViewCell
-        print(cell.name.text as! String)
+        //print(cell.name.text as! String)
         print(cell.placeId)
     }
     
