2ec0a5e9d7293f6e59acca3ff73ffe3e71717467
Barkha Bhojak <barkha@guest-wireless-upc-1606-10-120-099-219.usc.edu>
2018-04-23 19:31:15 -0700

    persistant data store

--- SearchIOS/AppDelegate.swift
+++ SearchIOS/AppDelegate.swift
@@ -20,29 +20,61 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         GMSPlacesClient.provideAPIKey("AIzaSyA41txth_QP4Nr-BU6Z3FBqJ8Awd9IMhMw")
         GMSServices.provideAPIKey("AIzaSyCv_Xf40KLucRM9cuTiinyRAvYt0ovNwaE")
+        if (UserDefaults.standard.object(forKey: "favorites") != nil) {
+            print("launching")
+            let a = self.window?.rootViewController?.childViewControllers[0] as! ViewController
+            a.favArray = UserDefaults.standard.array(forKey: "favorites") as! [[String : String]]
+            print(a.favArray)
+        }
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        print("applicationWillResignActive")
+        let a = self.window?.rootViewController?.childViewControllers[0] as! ViewController
+        let favArr = a.favArray
+        UserDefaults.standard.set(favArr, forKey: "favorites")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("applicationDidEnterBackground")
+        let a = self.window?.rootViewController?.childViewControllers[0] as! ViewController
+        let favArr = a.favArray
+        UserDefaults.standard.set(favArr, forKey: "favorites")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("applicationWillEnterForeground")
+        if (UserDefaults.standard.object(forKey: "favorites") != nil) {
+            print("launching")
+            let a = self.window?.rootViewController?.childViewControllers[0] as! ViewController
+            a.favArray = UserDefaults.standard.array(forKey: "favorites") as! [[String : String]]
+            print(a.favArray)
+        }
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("applicationDidBecomeActive")
+        if (UserDefaults.standard.object(forKey: "favorites") != nil) {
+            print("launching")
+            let a = self.window?.rootViewController?.childViewControllers[0] as! ViewController
+            a.favArray = UserDefaults.standard.array(forKey: "favorites") as! [[String : String]]
+            print(a.favArray)
+        }
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("applicationWillTerminate")
+        let a = self.window?.rootViewController?.childViewControllers[0] as! ViewController
+        let favArr = a.favArray
+        UserDefaults.standard.set(favArr, forKey: "favorites")
     }
 
 
